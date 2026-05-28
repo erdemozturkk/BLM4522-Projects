8fca2b94acd960df323f4119d68932df2062b2e6
erdemozturkk <merdemozturk19@gmail.com>
2026-05-28 23:09:25 +0300

    Test SSleri eklendi

--- Proje 2/Test.sql
+++ Proje 2/Test.sql
@@ -1,15 +1,16 @@
 USE AdventureWorks2016;
 GO
-
+/*
 -- 1. Kaç satır veri olduğunu gör 
 SELECT COUNT(*) FROM Sales.SalesOrderDetail;
 
--- 2. Tüm sipariş detaylarını siliyoruz!
+-- 2. Tüm sipariş detaylarını siliyoruz
 DELETE FROM Sales.SalesOrderDetail;
 
 -- 3. Tablo bomboş kalacak 
 SELECT COUNT(*) FROM Sales.SalesOrderDetail;
 GO
+*/
 
 
 /*
@@ -22,14 +23,13 @@ RESTORE LOG AdventureWorks2016
 FROM DISK = 'E:\SQL_Backups\AdventureWorks_Log.trn'
 WITH RECOVERY; -- RECOVERY diyerek veritabanını tekrar kullanıma açıyoruz
 GO
-
+*/
 
 ALTER DATABASE AdventureWorks2016 SET MULTI_USER;
 GO
 
 USE AdventureWorks2016;
 GO
--- Bakalım veriler geri gelmiş mi? 
+-- Veriler gelmiş mi diye kontrol ediyoruz
 SELECT COUNT(*) FROM Sales.SalesOrderDetail;
 GO
-*/
